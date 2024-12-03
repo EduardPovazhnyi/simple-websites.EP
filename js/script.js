5d2c86e12f5daa35e062733c1193cf58bec8f19e
Your Name <you@example.com>
2024-12-03 14:31:19 +0000

    intial coment

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 document.getElementById('alertButton').onclick = function () {
-    alert('Button clicked!');
+    alert("Thank you for your interest! We're excited to connect with you.");
 };
+document.getElementById("contactForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+    alert("Thank you for contacting us!");
+    this.reset();
+});
