5841744f43eef3c3b9e25eb1714fa65251703624
Eduard Povazhnyi <povaedik2002@gmail.com>
2025-01-14 13:40:48 +0000

    initial commit

--- php/Control_Structures.php
+++ php/Control_Structures.php
@@ -15,3 +15,11 @@ if ($score >= 70) {
     $result = calculateArea(5, 10);
     echo "The area is: " . $result; // Output: The area is: 50
 ?>
+
+<?php
+    function greet($name = "Guest") {
+        return "Hello, $name!";
+    }
+    echo greet(); //Output: Hello, Guest!
+    echo greet("Alice"); //Output: Hello, Alice!
+?>
