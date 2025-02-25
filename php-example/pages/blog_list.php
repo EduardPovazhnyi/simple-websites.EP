4dbefd623e85dd2af53433f3d22eaa5ae517465e
Your Name <you@example.com>
2025-02-25 14:05:57 +0000

    final 25/02/2025

--- php-example/pages/blog_list.php
+++ php-example/pages/blog_list.php
@@ -1,69 +1,39 @@
 <?php
 include 'database/config.php';
 include 'components/header.php';
+
+$blog = $conn->prepare("SELECT
+    id,
+    title,
+    image_url,
+    content,
+    created_at,
+    status
+FROM blog
+WHERE status = 'published' ");
+$blog->execute();
+$blog->store_result();
+$blog->bind_result($blogId, $blogTitle, $blogImg, $blogContent, $created_at, $status);
 ?>
-<div class="bg-gray-50 font-sans p-6">
-      <div class="grid grid-cols-1 lg:grid-cols-3 lg:gap-8 gap-12 items-center max-w-6xl max-lg:max-w-2xl max-sm:max-w-sm mx-auto">
-        <div>
-          <h2 class="text-4xl font-bold text-gray-300 uppercase mb-6">Blogs</h2>
-          <h2 class="text-3xl max-md:text-2xl font-extrabold text-gray-800 uppercase leading-10">Discover Our Letest Blog Posts</h2>
-        </div>
-        <div class="grid grid-cols-1 sm:grid-cols-2 gap-6 lg:col-span-2">
-          <div class="cursor-pointer rounded overflow-hidden group">
-            <img src="https://readymadeui.com/images/food.webp" alt="Blog Post 1" class="w-full h-52 object-cover rounded-md" />
-            <div class="py-6">
-              <span class="text-sm block text-gray-400 mb-2">10 FEB 2023 | BY JOHN DOE</span>
-              <h3 class="text-xl font-bold text-gray-800 group-hover:text-blue-500 transition-all">A Guide to Igniting Your Imagination</h3>
+<div class="bg-gray-100 md:px-10 px-4 py-12 font-[sans-serif]">
+      <div class="max-w-5xl max-lg:max-w-3xl max-sm:max-w-sm mx-auto">
+        <h2 class="text-3xl font-extrabold text-gray-800 mb-8">Latest Blog Posts</h2>
+        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 max-sm:gap-8">
+        <?php while ($blog->fetch()) : ?>
+          <div class="bg-white rounded overflow-hidden">
+           
+            <img src="<?= ROOT_DIR ?>assets/img/<?= $blogImg ?>" alt="Blog Post 1" class="w-full h-52 object-cover" />
+            <div class="p-6">
+              <h3 class="text-lg font-bold text-gray-800 mb-3"><?= $blogTitle ?></h3>
+              <p class="text-gray-500 text-sm"><?= $blogContent ?></p>
+              <p class="text-orange-500 text-[13px] font-semibold mt-4"><?= $status ?> <?= $created_at ?></p>
+              
+              <a href="javascript:void(0);" class="mt-4 inline-block px-4 py-2 rounded tracking-wider bg-orange-500 hover:bg-orange-600 text-white text-[13px]">Read More</a>
             </div>
           </div>
-          <div class="cursor-pointer rounded overflow-hidden group">
-            <img src="https://readymadeui.com/images/food11.webp" alt="Blog Post 2" class="w-full h-52 object-cover rounded-md" />
-            <div class="py-6">
-              <span class="text-sm block text-gray-400 mb-2">7 JUN 2023 | BY MARK ADAIR</span>
-              <h3 class="text-xl font-bold text-gray-800 group-hover:text-blue-500 transition-all">Hacks to Supercharge Your Day</h3>
-            </div>
-          </div>
-          <div class="cursor-pointer rounded overflow-hidden group">
-            <img src="https://readymadeui.com/images/food44.webp" alt="Blog Post 3" class="w-full h-52 object-cover rounded-md" />
-            <div class="py-6">
-              <span class="text-sm block text-gray-400 mb-2">5 OCT 2023 | BY SIMON KONECKI</span>
-              <h3 class="text-xl font-bold text-gray-800 group-hover:text-blue-500 transition-all">Trends and Predictions</h3>
-            </div>
-          </div>
-          <div class="cursor-pointer rounded overflow-hidden group">
-            <img src="https://readymadeui.com/images/food33.webp" alt="Blog Post 3" class="w-full h-52 object-cover rounded-md" />
-            <div class="py-6">
-              <span class="text-sm block text-gray-400 mb-2">10 DEC 2023 | BY SIMON KONECKI</span>
-              <h3 class="text-xl font-bold text-gray-800 group-hover:text-blue-500 transition-all">Innovators Changing the Game</h3>
-            </div>
-          </div>
-          <div class="cursor-pointer rounded overflow-hidden group">
-            <img src="https://readymadeui.com/images/food44.webp" alt="Blog Post 3" class="w-full h-52 object-cover rounded-md" />
-            <div class="py-6">
-              <span class="text-sm block text-gray-400 mb-2">5 OCT 2023 | BY SIMON KONECKI</span>
-              <h3 class="text-xl font-bold text-gray-800 group-hover:text-blue-500 transition-all">Trends and Predictions</h3>
-            </div>
-          </div>
-          <div class="cursor-pointer rounded overflow-hidden group">
-            <img src="https://readymadeui.com/images/food33.webp" alt="Blog Post 3" class="w-full h-52 object-cover rounded-md" />
-            <div class="py-6">
-              <span class="text-sm block text-gray-400 mb-2">10 DEC 2023 | BY SIMON KONECKI</span>
-              <h3 class="text-xl font-bold text-gray-800 group-hover:text-blue-500 transition-all">Innovators Changing the Game</h3>
-            </div>
-          </div>
-          <div class="cursor-pointer rounded overflow-hidden group">
-            <img src="https://readymadeui.com/images/food44.webp" alt="Blog Post 3" class="w-full h-52 object-cover rounded-md" />
-            <div class="py-6">
-              <span class="text-sm block text-gray-400 mb-2">5 OCT 2023 | BY SIMON KONECKI</span>
-              <h3 class="text-xl font-bold text-gray-800 group-hover:text-blue-500 transition-all">Trends and Predictions</h3>
-            </div>
-          </div>
-          <div class="cursor-pointer rounded overflow-hidden group">
-            <img src="https://readymadeui.com/images/food33.webp" alt="Blog Post 3" class="w-full h-52 object-cover rounded-md" />
-            <div class="py-6">
-              <span class="text-sm block text-gray-400 mb-2">10 DEC 2023 | BY SIMON KONECKI</span>
-              <h3 class="text-xl font-bold text-gray-800 group-hover:text-blue-500 transition-all">Innovators Changing the Game</h3>
-            </div>
+          <?php endwhile ?>         
+          
+          
           </div>
         </div>
       </div>
