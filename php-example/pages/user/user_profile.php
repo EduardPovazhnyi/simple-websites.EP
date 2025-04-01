71fa99e8459a98fa7ef0432629773b5e830fd188
Your Name <you@example.com>
2025-04-01 15:44:43 +0100

    Final 01/04/2025

--- php-example/pages/user/user_profile.php
+++ php-example/pages/user/user_profile.php
@@ -7,21 +7,7 @@ include 'components/sidebar.php';
 $user_id = $_SESSION['id'];
 echo $user_id;
 
-$blog = $conn->prepare("SELECT 
-    b.title, 
-    b.image_url, 
-    b.content, 
-    b.status, 
-    b.created_at, 
-    u.username,
-    u.profile_image -- Add profile_image column
-    
-FROM blog b 
-INNER JOIN users u ON b.author_id = u.id 
-WHERE b.id = ?");
-$blog->bind_param("i", $blogId); // Bind parameter to prevent SQL injection
-$blog->execute();
-$blog->store_result();
+
 
 // Prepare and execute the blog query
 $user_comments  = $conn->prepare("SELECT 
